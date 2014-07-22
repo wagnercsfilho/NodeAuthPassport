0c38d1e397cc95f9c1e54e19f7d22eac8504b837
Wagner CS Filho <wagnercsfilho@hotmail.com>
2014-07-22 18:20:45 -0300

    Login Twitter

--- app/routes.js
+++ app/routes.js
@@ -59,6 +59,19 @@ module.exports = function(app, passport){
 			failureRedirect : '/'
 		}));
 
+	// =====================================
+	// TWITTER ROUTES ======================
+	// =====================================
+	// route for twitter authentication and login
+	app.get('/auth/twitter', passport.authenticate('twitter'));
+
+	// handle the callback after twitter has authenticated the user
+	app.get('/auth/twitter/callback',
+		passport.authenticate('twitter', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
     // =====================================
 	// LOGOUT ==============================
 	// =====================================
